f4789513c2cce32d12eb2f7a81d1d1cf48046258
kangwenpiao <1564160583@qq.com>
2017-11-26 10:37:18 +0800

    图片类

---
+++ personal/src/main/java/top/kwp8/entity/Goodsimg.java
@@ -0,0 +1,27 @@
+package top.kwp8.entity;
+
+public class Goodsimg {
+
+	private Integer id;
+	private Integer goodsid;
+	private String img;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getGoodsid() {
+		return goodsid;
+	}
+	public void setGoodsid(Integer goodsid) {
+		this.goodsid = goodsid;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	
+}
